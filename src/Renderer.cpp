5c1abfb99e244ffe934183db63309bd091a7789a
Tarjan01 <743114342@qq.com>
2025-05-03 15:40:43 +0800

    shadow and jiter and filter

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -7,6 +7,8 @@
 #include "VecUtils.h"
 
 #include <limits>
+#include <random>
+#include <vector>
 
 
 Renderer::Renderer(const ArgParser &args) :
@@ -38,20 +40,101 @@ Renderer::Render()
             float ndcx = 2 * (x / (w - 1.0f)) - 1.0f;
             // Use PerspectiveCamera to generate a ray.
             // You should understand what generateRay() does.
-            Ray r = cam->generateRay(Vector2f(ndcx, ndcy));
+            if(_args.jitter) {
+                Vector3f color, n_val, d_val;
+                for(int i = 0;i < 16;i++) {
+                    std::random_device rd;
+                    std::mt19937 gen(rd());
+                    std::uniform_real_distribution<float> distr(-0.5, 0.5);
+                    float dx = distr(gen);
+                    float dy = distr(gen);
+                    float temp_ndcx = ndcx + 2 * dx / (w - 1.0f);
+                    float temp_ndcy = ndcy + 2 * dy / (h - 1.0f);
 
-            Hit h;
-            Vector3f color = traceRay(r, cam->getTMin(), _args.bounces, h);
+                    Ray r = cam->generateRay(Vector2f(temp_ndcx, temp_ndcy));
 
-            image.setPixel(x, y, color);
-            nimage.setPixel(x, y, (h.getNormal() + 1.0f) / 2.0f);
-            float range = (_args.depth_max - _args.depth_min);
-            if (range) {
-                dimage.setPixel(x, y, Vector3f((h.t - _args.depth_min) / range));
+                    Hit h;
+                    Vector3f color_new = traceRay(r, cam->getTMin(), _args.bounces, h);
+                    n_val += (h.getNormal() + 1.0f) / 2.0f;
+
+                    d_val += Vector3f(h.t - _args.depth_min);
+                    color += color_new;
+
+                    if (i == 15) {
+                        image.setPixel(x, y, color / 16);
+                        nimage.setPixel(x, y, n_val / 16);
+                        dimage.setPixel(x, y, d_val / 16 / (_args.depth_max - _args.depth_min));
+                    }
+                }
+            }
+            else {
+                Ray r = cam->generateRay(Vector2f(ndcx, ndcy));
+
+                Hit h;
+                Vector3f color = traceRay(r, cam->getTMin(), _args.bounces, h);
+
+                image.setPixel(x, y, color);
+                nimage.setPixel(x, y, (h.getNormal() + 1.0f) / 2.0f);
+                dimage.setPixel(x, y, Vector3f((h.t - _args.depth_min) / (_args.depth_max - _args.depth_min)));
             }
         }
     }
     // END SOLN
+
+    if (_args.filter) {
+        // Downsample the image using a Gaussian filter
+        Image downsample_image(w / 3, h / 3);
+        Image downsample_nimage(w / 3, h / 3);
+        Image downsample_dimage(w / 3, h / 3);
+        std::vector<float> gaussian_matrix = {1, 2, 1, 2, 4, 2, 1, 2, 1};//高斯核
+
+
+        for (int y = 0; y < h / 3; y++) {
+            for (int x = 0; x < w / 3; x++) {
+                std::vector<Vector3f> image_block;
+                Vector3f color;
+                for (int i = 0;i <= 2;i++) {
+                    for (int j = 0;j <= 2;j++) {
+                        image_block.push_back(image.getPixel(3 * x + i, 3 * y + j));
+                    }
+                }
+                for(int i = 0; i < 9; i++){
+                    color += image_block[i] * gaussian_matrix[i];
+                }
+                color = color / 16;
+                downsample_image.setPixel(x, y, color);
+
+                std::vector<Vector3f> nimage_block;
+                for(int i = 0;i <= 2;i++) {
+                    for (int j = 0;j <= 2;j++) {
+                        nimage_block.push_back(nimage.getPixel(3 * x + i, 3 * y + j));
+                    }
+                }
+                Vector3f ncolor;
+                for(int i = 0; i < 9; i++){
+                    ncolor += nimage_block[i] * gaussian_matrix[i];
+                }
+                ncolor = ncolor / 16;
+                downsample_nimage.setPixel(x, y, ncolor);
+
+                std::vector<Vector3f> dimage_block;
+                Vector3f dcolor;
+                for(int i = 0;i <= 2;i++) {
+                    for (int j = 0;j <= 2;j++) {
+                        dimage_block.push_back(dimage.getPixel(3 * x + i, 3 * y + j));
+                    }
+                }
+                for(int i = 0; i < 9; i++){
+                    dcolor += dimage_block[i] * gaussian_matrix[i];
+                }
+                dcolor = dcolor / 16;
+                downsample_dimage.setPixel(x, y, dcolor);   
+            }
+        }
+        image = downsample_image;
+        dimage = downsample_dimage;
+        nimage = downsample_nimage;
+    }
 
     // save the files 
     if (_args.output_file.size()) {
@@ -73,29 +156,48 @@ Vector3f Renderer::traceRay(const Ray &r,
     Hit &h) const
 {
     if (_scene.getGroup()->intersect(r, tmin, h)) {//有交点
-        Vector3f I,p;
+        Vector3f color,p;
         p = r.pointAtParameter(h.getT());
-        I= _scene.getAmbientLight() * h.getMaterial()->getDiffuseColor();
+        color= _scene.getAmbientLight() * h.getMaterial()->getDiffuseColor();
         for(auto& light:_scene.lights){
             Vector3f tolight, intensity;
             float distToLight;//必须为float，否则调用getIllumination()时出错
             light->getIllumination(p, tolight, intensity, distToLight);
             Vector3f ILight(0);//先不调用shade(),避免无效计算
+            if (_args.shadows) {
+                Vector3f shadowOrigin = p;
+                Ray shadowRay(shadowOrigin, tolight);
+                Hit shadowHit;
+                bool Intersected = _args.shadows&&_scene.getGroup()->intersect(shadowRay,0.0001f,shadowHit);//是否相交
+                double distToIntersection = (shadowRay.pointAtParameter(shadowHit.getT()) - shadowOrigin).abs();
+                if (Intersected&&distToIntersection<distToLight) {
+                }
+                else{
+                    ILight = h.getMaterial()->shade(r, h, tolight, intensity);
+                    color += ILight;
+                }
+            }
+            else{//没有阴影参数，正常计算
+                ILight = h.getMaterial()->shade(r, h, tolight, intensity);
+                color += ILight;
+            }
             ILight = h.getMaterial()->shade(r, h, tolight, intensity);
-            I += ILight;
+            color += ILight;
         }
         if(bounces>0){
-            Vector3f N = h.getNormal().normalized();//平面法向量
-            Vector3f temp_ray = r.getDirection().normalized();//反射前的光线
-            Vector3f newR = (temp_ray + 2 * Vector3f::dot(N,-temp_ray) * N).normalized();//反射后的光线
-            Ray newRay(p, newR);
+            Vector3f N = h.getNormal().normalized();
+            Vector3f E = r.getDirection().normalized();
+            Vector3f R = E - 2 * Vector3f::dot(N,E) * N;
+            R = R.normalized();
+            // Vector3f R = (temp_ray + 2 * Vector3f::dot(N,-temp_ray) * N).normalized();//反射后的光线
+            Ray newRay(p, R);
             // 创建新的光线对象，用于继续光线反射过程
             Hit new_h = Hit();
             Vector3f indirect = traceRay(newRay, 0.0001, bounces - 1, new_h);
             // 递归调用traceRay函数，计算间接光照
-            I += h.getMaterial()->getSpecularColor() * indirect;
+            color += h.getMaterial()->getSpecularColor() * indirect;
         }
-        return I;
+        return color;
     }
     return _scene.getBackgroundColor(r.getDirection());
     
